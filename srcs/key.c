b08c2e96ec066af72137222b3ca4fe1efa81cf9b
Martin Andre <marandre@w-r10-p3.s19.be>
2019-11-25 20:32:08 +0100

    Update speed to avoid slow down with textures

--- srcs/key.c
+++ srcs/key.c
@@ -6,7 +6,7 @@
 /*   By: marandre <marandre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 20:44:41 by marandre          #+#    #+#             */
-/*   Updated: 2019/11/25 12:15:58 by marandre         ###   ########.fr       */
+/*   Updated: 2019/11/25 12:28:47 by marandre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,11 @@ int		key_press(int keycode, t_cub3d *t)
 	else if (keycode == 53)
 		exit_program(t);
 	else if (keycode == 17)
+	{
+		t->ms = (t->ms == 0.08) ? 0.05 : 0.08;
+		t->rs = (t->rs == 0.075) ? 0.05 : 0.075;
 		t->texture = (t->texture == 1) ? 0 : 1;
+	}
 	return (0);
 }
 
