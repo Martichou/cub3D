62f2cea20bb8a744b327de13365fd43680604ec3
Martin Andre <marandre@e-r3-p2.s19.be>
2019-11-29 18:03:52 +0100

    Adjust pointer

--- srcs/fps.c
+++ srcs/fps.c
@@ -6,7 +6,7 @@
 /*   By: marandre <marandre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 14:42:06 by marandre          #+#    #+#             */
-/*   Updated: 2019/11/29 17:43:35 by marandre         ###   ########.fr       */
+/*   Updated: 2019/11/29 17:59:13 by marandre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,5 @@ void	fps(t_cub3d *t)
 		mlx_string_put(t->mlx, t->win, 10 + i, t->window_height - 10, 0x808080, "|");
 	mlx_string_put(t->mlx, t->win, t->window_width - 30, t->window_height - 10, 0xffffff, ft_itoa(t->bullets));
 	mlx_string_put(t->mlx, t->win, t->window_width - 30, t->window_height - 10, 0xffffff, " /8");
-	mlx_string_put(t->mlx, t->win, t->window_width / 2, t->window_height / 2, 0xffffff, ".");
+	mlx_string_put(t->mlx, t->win, t->window_width / 2, t->window_height / 2 + 15, 0xffffff, ".");
 }
