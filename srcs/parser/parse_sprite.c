921ebd811a6d2784548af59a9d3a2fd5a6272563
Martin Andre <marandre@w-r11-p3.s19.be>
2019-12-10 16:10:29 +0100

    Avoid segfault if textures is not found

--- srcs/parser/parse_sprite.c
+++ srcs/parser/parse_sprite.c
@@ -6,7 +6,7 @@
 /*   By: marandre <marandre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 21:27:41 by marandre          #+#    #+#             */
-/*   Updated: 2019/12/09 21:11:12 by marandre         ###   ########.fr       */
+/*   Updated: 2019/12/10 16:06:50 by marandre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,14 @@ int	parse_sprite_texture_10(t_cub3d *t, char *line)
 	a = 64;
 	b = 64;
 	t->tex[10].img = mlx_xpm_file_to_image(t->mlx, ft_strdup(line + 1), &a, &b);
-	t->tex[10].data = mlx_get_data_addr(t->tex[10].img, &t->tex[10].bpp,
+	if (t->tex[10].img)
+		t->tex[10].data = mlx_get_data_addr(t->tex[10].img, &t->tex[10].bpp,
 			&t->tex[10].sizeline, &t->tex[10].endian);
+	else
+	{
+		t->nb_lines = 0;
+		return (0);
+	}
 	return (1);
 }
 
@@ -33,7 +39,13 @@ int	parse_sprite_texture_11(t_cub3d *t, char *line)
 	a = 64;
 	b = 64;
 	t->tex[11].img = mlx_xpm_file_to_image(t->mlx, ft_strdup(line + 1), &a, &b);
-	t->tex[11].data = mlx_get_data_addr(t->tex[11].img, &t->tex[11].bpp,
+	if (t->tex[11].img)
+		t->tex[11].data = mlx_get_data_addr(t->tex[11].img, &t->tex[11].bpp,
 			&t->tex[11].sizeline, &t->tex[11].endian);
+	else
+	{
+		t->nb_lines = 0;
+		return (0);
+	}
 	return (1);
 }
--- srcs/parser/parse_textures.c
+++ srcs/parser/parse_textures.c
@@ -6,11 +6,12 @@
 /*   By: marandre <marandre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 20:48:07 by marandre          #+#    #+#             */
-/*   Updated: 2019/12/09 21:24:26 by marandre         ###   ########.fr       */
+/*   Updated: 2019/12/10 16:06:27 by marandre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+#include <stdio.h>
 
 int	parse_north_texture(t_cub3d *t, char *line)
 {
@@ -20,8 +21,14 @@ int	parse_north_texture(t_cub3d *t, char *line)
 	a = 64;
 	b = 64;
 	t->tex[0].img = mlx_xpm_file_to_image(t->mlx, ft_strdup(line + 1), &a, &b);
-	t->tex[0].data = mlx_get_data_addr(t->tex[0].img, &t->tex[0].bpp,
+	if (t->tex[0].img)
+		t->tex[0].data = mlx_get_data_addr(t->tex[0].img, &t->tex[0].bpp,
 			&t->tex[0].sizeline, &t->tex[0].endian);
+	else
+	{
+		t->nb_lines = 0;
+		return (0);
+	}
 	return (1);
 }
 
@@ -33,8 +40,14 @@ int	parse_south_texture(t_cub3d *t, char *line)
 	a = 64;
 	b = 64;
 	t->tex[1].img = mlx_xpm_file_to_image(t->mlx, ft_strdup(line + 1), &a, &b);
-	t->tex[1].data = mlx_get_data_addr(t->tex[1].img, &t->tex[1].bpp,
+	if (t->tex[1].img)
+		t->tex[1].data = mlx_get_data_addr(t->tex[1].img, &t->tex[1].bpp,
 			&t->tex[1].sizeline, &t->tex[1].endian);
+	else
+	{
+		t->nb_lines = 0;
+		return (0);
+	}
 	return (1);
 }
 
@@ -46,8 +59,14 @@ int	parse_west_texture(t_cub3d *t, char *line)
 	a = 64;
 	b = 64;
 	t->tex[2].img = mlx_xpm_file_to_image(t->mlx, ft_strdup(line + 1), &a, &b);
-	t->tex[2].data = mlx_get_data_addr(t->tex[2].img, &t->tex[2].bpp,
+	if (t->tex[2].img)
+		t->tex[2].data = mlx_get_data_addr(t->tex[2].img, &t->tex[2].bpp,
 			&t->tex[2].sizeline, &t->tex[2].endian);
+	else
+	{
+		t->nb_lines = 0;
+		return (0);
+	}
 	return (1);
 }
 
@@ -59,8 +78,14 @@ int	parse_east_texture(t_cub3d *t, char *line)
 	a = 64;
 	b = 64;
 	t->tex[3].img = mlx_xpm_file_to_image(t->mlx, ft_strdup(line + 1), &a, &b);
-	t->tex[3].data = mlx_get_data_addr(t->tex[3].img, &t->tex[3].bpp,
+	if (t->tex[3].img)
+		t->tex[3].data = mlx_get_data_addr(t->tex[3].img, &t->tex[3].bpp,
 			&t->tex[3].sizeline, &t->tex[3].endian);
+	else
+	{
+		t->nb_lines = 0;
+		return (0);
+	}
 	return (1);
 }
 
@@ -72,7 +97,13 @@ int	setup_sky(t_cub3d *t)
 	a = 64;
 	b = 64;
 	t->tex[4].img = mlx_xpm_file_to_image(t->mlx, "textures/sky.xpm", &a, &b);
-	t->tex[4].data = mlx_get_data_addr(t->tex[4].img, &t->tex[4].bpp,
+	if (t->tex[4].img)
+		t->tex[4].data = mlx_get_data_addr(t->tex[4].img, &t->tex[4].bpp,
 			&t->tex[4].sizeline, &t->tex[4].endian);
+	else
+	{
+		t->nb_lines = 0;
+		return (0);
+	}
 	return (1);
 }
